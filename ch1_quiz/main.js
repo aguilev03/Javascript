25a46f769eb330196650d519d6757ac53347e2f2
Evan <evan@eaguil.com>
2017-12-25 13:25:45 -0600

    making it work

--- ch1_quiz/main.js
+++ ch1_quiz/main.js
@@ -2,6 +2,6 @@ alert('Welcome to Quiz Ninja!');
 
 const question = "What is Superman's real name?"
 const answer = prompt(question);
-alert("You answered ${answer}");
+alert("You answered  ",answer);
 
 
